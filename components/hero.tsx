113df5982a6a994c8b2be33cb514ef0fbd825566
Archies13 <archies.patil13@gmail.com>
2025-06-21 14:56:07 -0400

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -62,7 +62,9 @@ export default function Hero() {
           Seasoned project manager with 8+ years driving results in project, product, and operations management across diverse industries. From startups to large enterprises, I’ve managed portfolios over $10M with a focus on innovation, accountability, and measurable results. Specialized in leading cross-functional teams and executing complex projects with precision and impact.
          
           <strong className="font-semibold text-gray-800 dark:text-gray-200">
+            
            Project Management | Product Management | Operations Management | People Management | Customer Relationship Management
+            
           </strong>
 
           </motion.p>
